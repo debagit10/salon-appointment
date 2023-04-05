114bfde2950d51a91dff26bd7001754db25001d0
Josiah Elias <85748795+debagit10@users.noreply.github.com>
2023-04-05 18:23:52 +0100

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -4,10 +4,6 @@ PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 echo -e "\n~~~~~ MY SALON ~~~~~\n"
 
 SERVICE_MENU () {
-  if [[ $1 ]]
-    then 
-      echo $1
-  fi
   echo -e "Welcome to My Salon, how can I help you?\n" 
   SERVICES=$($PSQL "SELECT service_id,name FROM services ORDER BY service_id")
   echo "$SERVICES" | while read SERVICE_ID BAR SERVICE
@@ -42,9 +38,16 @@ CUSTOMER_MENU() {
       read SERVICE_TIME
       echo -e "\nI have put you down for a cut at $SERVICE_TIME, $CUSTOMER_NAME."
       INSERT_CUSTOMER=$($PSQL "INSERT INTO customers(phone,name) VALUES('$CUSTOMER_PHONE','$CUSTOMER_NAME')")
+      APPOINTMENT
       else
-         
+      APPOINTMENT
     fi
+}
+
+APPOINTMENT() {
+  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+  SERVICE_ID=$($PSQL "SELECT service_id FROM services WHERE service_id = $SERVICE_ID_SELECTED")
+  INSERT_APPOINMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id,time) VALUES($CUSTOMER_ID,$SERVICE_ID,'$SERVICE_TIME')")
 }
 
 SERVICE_MENU
